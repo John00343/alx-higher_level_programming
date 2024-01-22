9d6ee8104be680c7dabac4ea82d7a5ae6cc6b5fe
john00343 <johnwmacharia8@gmail.com>
2024-01-22 10:49:19 -0800

    Errors and Exceptions

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -21,7 +21,7 @@ void print_python_list(PyObject *p)
     fflush(stdout);
 
     printf("[*] Python list info\n");
-    if (strcmp(Py_TYPE(p)->tp_name, "list") != 0)
+    if (strncmp(Py_TYPE(p)->tp_name, "list", 4) != 0)
     {
         printf("  [ERROR] Invalid List Object\n");
         return;
@@ -34,9 +34,54 @@ void print_python_list(PyObject *p)
     {
         type = Py_TYPE(list->ob_item[i])->tp_name;
         printf("Element %ld: %s\n", i, type);
-        if (strcmp(type, "bytes") == 0)
+
+        if (strncmp(type, "bytes", 5) == 0)
             print_python_bytes(list->ob_item[i]);
-        else if (strcmp(type, "float") == 0)
+        else if (strncmp(type, "float", 5) == 0)
             print_python_float(list->ob_item[i]);
     }
+}
+
+/**
+ * print_python_bytes - Prints basic info about Python bytes.
+ * @p: A PyObject bytes object.
+ */
+void print_python_bytes(PyObject *p)
+{
+    Py_ssize_t size, i;
+    char *str;
+
+    printf("[.] bytes object info\n");
+    if (strncmp(Py_TYPE(p)->tp_name, "bytes", 5) != 0)
+    {
+        printf("  [ERROR] Invalid Bytes Object\n");
+        return;
+    }
+
+    size = PyBytes_Size(p);
+    printf("  size: %ld\n", size);
+
+    str = PyBytes_AsString(p);
+    printf("  trying string: %s\n", str);
+
+    printf("  first 10 bytes: ");
+    for (i = 0; i < size && i < 10; i++)
+        printf("%02x ", (unsigned char)str[i]);
+    printf("\n");
+}
+
+/**
+ * print_python_float - Prints basic info about Python float.
+ * @p: A PyObject float object.
+ */
+void print_python_float(PyObject *p)
+{
+    printf("[.] float object info\n");
+    if (strncmp(Py_TYPE(p)->tp_name, "float", 5) != 0)
+    {
+        printf("  [ERROR] Invalid Float Object\n");
+        return;
+    }
+
+    printf("  value: %f\n", PyFloat_AsDouble(p));
 }
