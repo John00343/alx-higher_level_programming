2dd75956a0406fc0cafa124bc10b7ede4762b1c6
john00343 <johnwmacharia8@gmail.com>
2024-01-22 10:17:06 -0800

    Errors and Exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
+def safe_print_list(my_list=None, x=0):
+    if my_list is None:
+        my_list = []
+    
     num = 0
     for i in range(x):
         try:
@@ -8,4 +11,4 @@ def safe_print_list(my_list=[], x=0):
         except IndexError:
             break
     print("")
-    Return
+    return
